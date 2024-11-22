b9a49b0a3b6dc5de21cdbfe4329007111ab0528e
dewikhulbi <dewikhlb20@gmail.com>
2024-11-22 12:16:50 +0700

    membuat enum class untuk mempermudah definisi rute navigasi di aplikasi.

---
+++ app/src/main/java/com/example/meet8/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,28 @@
+package com.example.meet8.Navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.meet8.ui.view.screen.DatailView
+import com.example.meet8.ui.view.screen.MahasiswaFormView
+import com.example.meet8.ui.view.screen.RencanaStudyView
+import com.example.meet8.ui.view.screen.SplashView
+import com.example.meet8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.meet8.ui.view.viewmodel.RencanaStudyViewModel
+
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
