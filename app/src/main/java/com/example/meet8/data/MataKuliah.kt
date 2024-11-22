ba559436213b659a0f7bd9cfcbc74cf67a678d23
dewikhulbi <dewikhlb20@gmail.com>
2024-11-22 12:12:16 +0700

    untuk membuat daftar mata kuliah yang diinginkan

---
+++ app/src/main/java/com/example/meet8/data/MataKuliah.kt
@@ -0,0 +1,13 @@
+package com.example.meet8.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+
+    )
+}
